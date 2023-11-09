c97b250b19ff26e7231c41a0b433404d698728e3
Bar Volovski <bar.volovski@post.runi.ac.il>
2023-11-09 16:38:42 +0200

    diagram of models

--- chat.py
+++ chat.py
@@ -8,23 +8,35 @@ app = Flask(__name__, static_url_path='', static_folder='static')
 
 # Define the function to process the conversation data and calculate the costs
 def process_conversation_data_from_data(conversations_data, price_per_1000_prompt_tokens, price_per_1000_sampled_tokens, start_date=None, end_date=None):
+    model_counts = {}  # Dictionary to keep track of token counts per model
     # Function to count prompt and sampled tokens from a conversation
     def count_prompt_and_sampled_tokens(conversation):
         prompt_token_count = 0
         sampled_token_count = 0
+        
+
         for node_id, node in conversation['mapping'].items():
             message = node.get('message')
             if message and 'content' in message and 'author' in message:
                 author_role = message['author'].get('role')
+                model_slug = message['metadata'].get('model_slug', 'unknown')  # Default to 'unknown' if not present
                 parts = message['content'].get('parts', [])
+                
+                # Initialize the model counts if not already done
+                if model_slug not in model_counts:
+                    model_counts[model_slug] = {'prompt_token_count': 0, 'sampled_token_count': 0}
+
                 for part in parts:
                     if isinstance(part, str):
                         # Tokenize the part using NLTK's word_tokenize
                         tokens = word_tokenize(part.strip())
                         if author_role == 'user':
                             prompt_token_count += len(tokens)
+                            model_counts[model_slug]['prompt_token_count'] += len(tokens)
                         elif author_role == 'assistant':
                             sampled_token_count += len(tokens)
+                            model_counts[model_slug]['sampled_token_count'] += len(tokens)
+        
         return prompt_token_count, sampled_token_count
 
 
@@ -56,8 +68,8 @@ def process_conversation_data_from_data(conversations_data, price_per_1000_promp
 
     # Calculate the average monthly cost
     average_monthly_cost = monthly_tokens_df['total_cost'].mean()
-
-    return monthly_tokens_df, average_monthly_cost
+    print(model_counts.keys())
+    return monthly_tokens_df, average_monthly_cost, model_counts
 
 # The route for the main page with the upload form
 @app.route('/')
@@ -80,12 +92,13 @@ def upload_file():
     if file:
         # Read the content of the file
         conversations_data = json.load(file.stream)
-        monthly_costs_df, avg_monthly_cost = process_conversation_data_from_data(
+        monthly_costs_df, avg_monthly_cost, model_counts = process_conversation_data_from_data(
             conversations_data,
             price_per_1000_prompt_tokens=0.03,  # in dollars
             price_per_1000_sampled_tokens=0.06,  # in dollars
             start_date=start_date,  # Start date from the form
             end_date=end_date       # End date from the form
+
             
         )
         
@@ -106,6 +119,16 @@ def upload_file():
         labels = [record['date'].strftime('%B %Y') for record in savings_analysis]
         data = [record['savings'] for record in savings_analysis]
 
+        # Prepare the data for the diagram
+        model_totals = {model: counts['prompt_token_count'] + counts['sampled_token_count'] 
+                    for model, counts in model_counts.items()}
+        grand_total = sum(model_totals.values())
+        model_usage_labels = list(model_totals.keys())
+        model_usage_data = [(count / grand_total) * 100 for count in model_totals.values()]  # Convert to percentages
+
+        print(model_usage_data)
+        print(model_usage_labels)
+
         # Render the results in a new template
         return render_template('results.html', 
                                results_table=results_html, 
@@ -115,7 +138,9 @@ def upload_file():
                                chart_labels=labels,
                                chart_data=data,
                                start_date=start_date,  # Pass start date to the template
-                               end_date=end_date       # Pass end date to the template
+                               end_date=end_date,       # Pass end date to the template
+                               model_usage_labels=model_usage_labels,
+                               model_usage_data=model_usage_data
                                )
 
 if __name__ == '__main__':
--- static/js/script.js
+++ static/js/script.js
@@ -1,37 +1,87 @@
 document.addEventListener("DOMContentLoaded", function() {
     var toggleButton = document.getElementById("toggleDetails");
     var detailsContent = document.getElementById("detailsContent");
-    var chartContainer = document.getElementById("chartContainer");
-  
+    var barChartContainer = document.getElementById("chartContainer");
+    var pieChartContainer = document.getElementById("pieChartContainer");
+
     // Event listener for the toggle button
-    toggleButton.addEventListener("click", function() {
-      var isHidden = chartContainer.style.display === 'none';
-      chartContainer.style.display = isHidden ? 'block' : 'none';
-      detailsContent.style.display = isHidden ? 'block' : 'none';
-      toggleButton.textContent = isHidden ? 'Hide Details' : 'Show Details';
-    });
-  
-    // Initialize the chart with data passed from Flask
-    var ctx = document.getElementById('detailsChart').getContext('2d');
-    var detailsChart = new Chart(ctx, {
-        type: 'bar', // Change to 'line', 'doughnut', etc. as desired
-        data: {
-            labels: chartLabels,
-            datasets: [{
-                label: 'Monthly Savings',
-                data: chartData,
-                backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                borderColor: 'rgba(54, 162, 235, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
+    // Event listener for the toggle button
+toggleButton.addEventListener("click", function() {
+    var isHidden = detailsContent.style.display === 'none';
+    detailsContent.style.display = isHidden ? 'block' : 'none';
+    barChartContainer.style.display = isHidden ? 'block' : 'none';
+    pieChartContainer.style.display = isHidden ? 'block' : 'none';
+    toggleButton.textContent = isHidden ? 'Hide Details' : 'Show Details';
+});
+
+
+    // Initialize the bar chart if the container exists
+    if (barChartContainer) {
+        var ctx = document.getElementById('detailsChart').getContext('2d');
+        var detailsChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: chartLabels,
+                datasets: [{
+                    label: 'Monthly Savings',
+                    data: chartData,
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
                 }
             }
-        }
-    });
-  });
-  
+        });
+    }
+
+    // Initialize the pie chart if the container exists
+    if (pieChartContainer) {
+        var pieCtx = document.getElementById('modelUsageChart').getContext('2d');
+        var modelUsageChart = new Chart(pieCtx, {
+            type: 'pie',
+            data: {
+                labels: modelUsageLabels,
+                datasets: [{
+                    data: modelUsageData,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Model Usage'
+                },
+                animation: {
+                    animateScale: true,
+                    animateRotate: true
+                }
+            }
+        });
+    }
+});
